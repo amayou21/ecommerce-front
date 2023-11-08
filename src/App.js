427471f5de7eabe61fd13ceee2ce9ca454c41abc
amayou21 <amaguaryoussefpay10@gmail.com>
2023-11-08 16:18:56 +0100

    vv

--- src/App.js
+++ src/App.js
@@ -1,58 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
-import Home from './pages/Home/Home';
-import { DarktMode, LightMode } from './component/utility/AppMode';
-import NavBar from './component/utility/NavBar';
-import Footer from './component/utility/Footer'
-import LoginPage from './pages/Auth/Login';
-import Register from './pages/Auth/RegisterPage';
-import AllCategoryPage from './pages/categories/AllCategoryPage';
-import AllBrandsPage from './pages/Brands/AllBrandsPage';
-import ShopProductPage from './pages/Products/ShopProductPage';
-import ProductDetailsPage from './pages/Products/ProductDetailsPage'
-import CartPage from './pages/cart/CartPage';
-import ChoosePaymentMethod from './pages/payment/ChoosePaymentMethod';
-import AdminDashPage from './pages/Admin/AdminDashPage';
-import AllAdminProducts from './pages/Admin/AllAdminProducts';
-import AllAdminOrders from './pages/Admin/AllAdminOrders';
-import OrderDetaillsPage from './pages/Admin/OrderDetaillsPage';
-import AddBrandPage from './pages/Admin/AddBrandPage';
-import AddCategoryPage from './pages/Admin/AddCategoryPage';
-import Dashboard from './component/Admin/Dashboard';
-import AddSubCategoryPage from './pages/Admin/AddSubCategoryPage';
-import AddProductPage from './pages/Admin/AddProductPage';
-import UserDashPage from './pages/User/UserDashPage';
-import UserDashboard from './component/user/UserDashboard';
-import AllUserAddressPage from './pages/User/AddUserAddressPage';
-import AllUserOrdersPage from './pages/User/AllUserOrdersPage';
-import AllUserFavoritsPage from './pages/User/AllUserFavoritsPage';
-import ProfilePage from './pages/User/ProfilePage';
-import UserAddAddressPage from './pages/User/UserAddAddressPage';
-import UserEditAddress from './pages/User/UserEditAddress';
+import Home from "./pages/Home/Home";
+import { DarktMode, LightMode } from "./component/utility/AppMode";
+import NavBar from "./component/utility/NavBar";
+import Footer from "./component/utility/Footer";
+import LoginPage from "./pages/Auth/Login";
+import Register from "./pages/Auth/RegisterPage";
+import AllCategoryPage from "./pages/categories/AllCategoryPage";
+import AllBrandsPage from "./pages/Brands/AllBrandsPage";
+import ShopProductPage from "./pages/Products/ShopProductPage";
+import ProductDetailsPage from "./pages/Products/ProductDetailsPage";
+import CartPage from "./pages/cart/CartPage";
+import ChoosePaymentMethod from "./pages/payment/ChoosePaymentMethod";
+import AdminDashPage from "./pages/Admin/AdminDashPage";
+import AllAdminProducts from "./pages/Admin/AllAdminProducts";
+import AllAdminOrders from "./pages/Admin/AllAdminOrders";
+import OrderDetaillsPage from "./pages/Admin/OrderDetaillsPage";
+import AddBrandPage from "./pages/Admin/AddBrandPage";
+import AddCategoryPage from "./pages/Admin/AddCategoryPage";
+import Dashboard from "./component/Admin/Dashboard";
+import AddSubCategoryPage from "./pages/Admin/AddSubCategoryPage";
+import AddProductPage from "./pages/Admin/AddProductPage";
+import UserDashPage from "./pages/User/UserDashPage";
+import UserDashboard from "./component/user/UserDashboard";
+import AllUserAddressPage from "./pages/User/AddUserAddressPage";
+import AllUserOrdersPage from "./pages/User/AllUserOrdersPage";
+import AllUserFavoritsPage from "./pages/User/AllUserFavoritsPage";
+import ProfilePage from "./pages/User/ProfilePage";
+import UserAddAddressPage from "./pages/User/UserAddAddressPage";
+import UserEditAddress from "./pages/User/UserEditAddress";
+import AmaguarCv from "./component/utility/AmaguarCv"
 
 const App = () => {
-
-  const [mode, setMode] = useState(localStorage.getItem("currentTheme") === null
-    ? "light"
-    : localStorage.getItem("currentTheme") === "light"
+  const [mode, setMode] = useState(
+    localStorage.getItem("currentTheme") === null
+      ? "light"
+      : localStorage.getItem("currentTheme") === "light"
       ? "light"
       : "dark"
-  )
+  );
 
   const darkTheme = createTheme({
     palette: {
       mode,
-      ...(mode === 'light'
-        ?
-        LightMode
-        :
-        DarktMode
-
-      ),
+      ...(mode === "light" ? LightMode : DarktMode),
     },
   });
 
@@ -60,15 +55,11 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <BrowserRouter>
-        <NavBar setMode={setMode} />
-        <div className=' mt-[56px] md:mt-[64px] xl:mt-[64px]'>
-          {/* <div> */}
-
-
-
+        {/* <NavBar setMode={setMode} /> */}
+        {/* <div className=' mt-[56px] md:mt-[64px] xl:mt-[64px]'> */}
+        <div>
           <Routes>
-
-            <Route index element={<Home setMode={setMode} />} />
+            {/* <Route index element={<Home setMode={setMode} />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/register' element={<Register />} />
             <Route path='/allcategory' element={<AllCategoryPage />} />
@@ -105,14 +96,13 @@ const App = () => {
               <Route path="add-address" element={<UserAddAddressPage />} />
               <Route path="edit-address" element={<UserEditAddress />} />
             </Route>
-
+*/}
+            <Route path="/" element={<AmaguarCv />} />
           </Routes>
-
-
-        </div >
+        </div>
       </BrowserRouter>
-      <Footer />
+      {/* <Footer /> */}
     </ThemeProvider>
   );
-}
+};
 export default App;
